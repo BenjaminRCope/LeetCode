0fb0b5a8d062d763c564386c5ed065a16009079d
Benjamin Cope <brc1379@gmail.com>
2022-05-27 18:12:20 -0600

    Time: 183 ms (21.37%), Space: 42.5 MB (61.06%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -3,20 +3,7 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-// //     iterate over first num
-// //     iterate over second num
-//     for (let i = 0; i < nums.length; i+=1) {
-//         let firstInt = nums[i];
-//         for(let j = 0; j < nums.length; j+=1) {
-//             let secondInt = nums[j];
-            
-//             if (firstInt + secondInt === target && i !== j) {
-//                 return [i, j];
-//             }
-//         }
-//     }
-    
+var twoSum = function(nums, target) {   
     for (let i = 0; i < nums.length; i+=1) {         
         for (let j = i; j < nums.length; j+=1) {
             if (nums[i] + nums[j] === target && i !== j) {
